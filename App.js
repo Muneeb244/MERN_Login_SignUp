823183ea5d3fcffea2f9da3ecb854e1779ca82ce
Muneeb Ahmad <91079744+Muneeb244@users.noreply.github.com>
2023-03-30 02:07:15 +0500

    Completed UI and implemeted backend for tasks(get and post)

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ export default function App() {
     <NavigationContainer>
       <StartNavigation />
     </NavigationContainer>
+    // <Todo />
   );
 }
 
--- src/screens/Todo.js
+++ src/screens/Todo.js
@@ -1,31 +1,75 @@
-import { StyleSheet, Text, View, TextInput, FlatList, Pressable } from "react-native";
-import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+  Pressable,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 
 import form from "../styles/form";
 import ErrorMessage from "../components/ErrorMessage";
 import { MaterialIcons } from "@expo/vector-icons";
 import Task from "../components/Task";
 
-const data = [
-  { id: 1, task: "Social app", completed: false },
+const info = [
+  { id: 1, task: "Social app", completed: true },
   { id: 2, task: "Social app1", completed: false },
   { id: 3, task: "Social app2", completed: true },
 ];
 
-
-
 const Todo = ({ route }) => {
-    
-    const [activeButton, setActiveButton] = useState(1);
-    
-    const all = () => {
-        setActiveButton(1);
-    }
-    
-    const completed = () => {
-        setActiveButton(2);
-        // return data.filter((item) => item.completed);
-    }
+  const [input, setInput] = useState("");
+  const [activeButton, setActiveButton] = useState(1);
+  const [data, setData] = useState([]);
+
+  const getTodo = () => {
+    fetch("http://192.168.0.128:3000/todo", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({ token: route.params.token }),
+    })
+      .then((res) => res.json())
+      .then((tData) => {
+        setData(tData);
+      })
+      .catch((e) => console.log("Error", e));
+  };
+
+  const sendToBackend = () => {
+    fetch("http://192.168.0.128:3000/todo/add", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+      body: JSON.stringify({ token: route.params.token, todo: input }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if(data) {
+          setData(data);
+          setInput("");
+        }
+      })
+      .catch((e) => console.log("Error", e));
+  };
+
+  useEffect(() => {
+    getTodo();
+  }, []);
+
+  const all = () => {
+    setActiveButton(1);
+    return getTodo();
+  };
+
+  const completed = () => {
+    setActiveButton(2);
+    return setData(data.filter((item) => item.completed));
+  };
 
   return (
     <View style={styles.screen}>
@@ -34,13 +78,18 @@ const Todo = ({ route }) => {
           name="todo"
           style={[form.input, { flex: 1, marginRight: 5 }]}
           placeholder="Add your task"
-          onChangeText={(val) => console.log(val)}
+          onChangeText={setInput}
         />
         <MaterialIcons
           name="add-circle"
           size={45}
           color="#F80053"
-          onPress={() => console.log(route.params?.token)}
+          onPress={() => {
+            // data.push({ id: data.length + 1, task: input, completed: false });
+            // setData(data);
+            // console.log(data);
+            sendToBackend();
+          }}
         />
         {/* <ErrorMessage error={} visible={false} /> */}
       </View>
@@ -48,18 +97,44 @@ const Todo = ({ route }) => {
         <FlatList
           data={data}
           renderItem={({ item }) => (
-            <Task task={item.task} completed={item.completed} />
+            <Task task={item.todo} completed={item.completed} />
           )}
-          keyExtractor={(item) => item.id.toString()}
+          keyExtractor={(item) => item._id}
           style={styles.list}
         />
       </View>
       <View style={styles.footer}>
-        <Pressable style={[styles.footerbtn, {backgroundColor: activeButton == 1 ? '#F80053' : '#fff'}]} onPress={all}>
-            <Text style={[styles.footertext, {color: activeButton == 1 ? '#fff' : '#F80053'}]}>All</Text>
+        <Pressable
+          style={[
+            styles.footerbtn,
+            { backgroundColor: activeButton == 1 ? "#F80053" : "#fff" },
+          ]}
+          onPress={all}
+        >
+          <Text
+            style={[
+              styles.footertext,
+              { color: activeButton == 1 ? "#fff" : "#F80053" },
+            ]}
+          >
+            All
+          </Text>
         </Pressable>
-        <Pressable style={[styles.footerbtn, {backgroundColor: activeButton == 2 ? '#F80053' : '#fff'}]} onPress={completed}>
-            <Text style={[styles.footertext, {color: activeButton == 2 ? '#fff' : '#F80053'}]}>Completed</Text>
+        <Pressable
+          style={[
+            styles.footerbtn,
+            { backgroundColor: activeButton == 2 ? "#F80053" : "#fff" },
+          ]}
+          onPress={completed}
+        >
+          <Text
+            style={[
+              styles.footertext,
+              { color: activeButton == 2 ? "#fff" : "#F80053" },
+            ]}
+          >
+            Completed
+          </Text>
         </Pressable>
       </View>
     </View>
@@ -87,31 +162,31 @@ const styles = StyleSheet.create({
   listParent: {
     width: "100%",
     // backgroundColor: "blue",
-    height: '75%',
-    justifyContent: 'center',
-    alignItems: 'center',
+    height: "75%",
+    justifyContent: "center",
+    alignItems: "center",
   },
   footer: {
-    width: '100%',
-    height: '10%',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-},
-footerbtn: {
-    backgroundColor: '#fff',
+    width: "100%",
+    height: "10%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footerbtn: {
+    backgroundColor: "#fff",
     marginHorizontal: 10,
     padding: 10,
-    width: '40%',
+    width: "40%",
     borderWidth: 2,
-    borderColor: '#F80053',
+    borderColor: "#F80053",
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   footertext: {
-    color: '#F80053',
+    color: "#F80053",
     fontSize: 15,
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+  },
 });
